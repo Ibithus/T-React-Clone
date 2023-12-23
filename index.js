32935f8c5b5fe147ce8e2ce0f2ef9a1654b3c493
Ibrahim <ibitheboss786@outlook.com>
2023-12-23 17:42:09 +0000

    Adding Hamburger Menu From Other Project Code

---
+++ index.js
@@ -0,0 +1,9 @@
+// This Is To Add Functionality To Make The Hamburger Menu
+
+function openMenu () {
+    document.body.classList += " Menu-Open"
+}
+
+function closeMenu () {
+    document.body.classList.remove("Menu-Open") 
+}
