2cafffa08975f7427b92e63a591923599d0bcf83
Ibrahim <ibitheboss786@outlook.com>
2023-12-23 18:07:57 +0000

    Quick Format

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // This Is To Add Functionality To Make The Hamburger Menu
 
-function openMenu () {
+function openMenu() {
     document.body.classList += " Menu-Open"
 }
 
-function closeMenu () {
-    document.body.classList.remove("Menu-Open") 
+function closeMenu() {
+    document.body.classList.remove("Menu-Open")
 }
